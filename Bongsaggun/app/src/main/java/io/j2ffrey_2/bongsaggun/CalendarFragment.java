0b80e3bbb3ad82b1bf27c5d0a9bfe50ee0dcd74e
DongHee Kim <opklnm102@gmail.com>
2015-10-02 03:06:21 +0900

    MainActivity 구현

---
+++ Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/CalendarFragment.java
@@ -0,0 +1,42 @@
+package io.j2ffrey_2.bongsaggun;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalendarFragment extends Fragment {
+
+    public static final String TAG = "CalendarFragment";
+
+    public static CalendarFragment newInstance() {
+        CalendarFragment fragment = new CalendarFragment();
+
+        return fragment;
+    }
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+
+
+
+
+}
---
+++ Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/MyPageFragment.java
@@ -0,0 +1,41 @@
+package io.j2ffrey_2.bongsaggun;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class MyPageFragment extends Fragment {
+
+    public static MyPageFragment newInstance() {
+        MyPageFragment fragment = new MyPageFragment();
+
+        return fragment;
+    }
+
+    public MyPageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_page, container, false);
+    }
+
+
+
+
+}
