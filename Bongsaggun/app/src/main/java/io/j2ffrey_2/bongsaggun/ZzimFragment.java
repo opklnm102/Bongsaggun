dbf31a18e75e0f38573578f6241c4969736c3367
andasom <dksektha94@naver.com>
2015-10-29 19:00:23 +0900

    #4 찜봉사 레이아웃 구현

--- Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/ZzimFragment.java
+++ Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/ZzimFragment.java
@@ -1,14 +1,25 @@
 package io.j2ffrey_2.bongsaggun;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ZzimFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ZzimListAdapter adapter;
 
     public static ZzimFragment newInstance() {
         ZzimFragment fragment = new ZzimFragment();
@@ -20,6 +31,24 @@ public class ZzimFragment extends Fragment {
         // Required empty public constructor
     }
 
+    //getdata
+    public static List<Zzim_list_contents> getData(){
+        List<Zzim_list_contents> data = new ArrayList<>();
+        Log.i("MyActivity", "MyClass.getView() — get item number ");
+        String[] titles = {"봉사꾼1" , "봉사꾼2" };
+        //  String[] iconid = {"봉사꾼1" , "봉사꾼2" };
+        for (int i=0; i<titles.length; i++){
+            Zzim_list_contents current = new Zzim_list_contents();
+            current.title = titles[i];
+            current.IconId = R.mipmap.ic_launcher;
+            data.add(current);
+        }
+
+        return data;
+    }
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +58,16 @@ public class ZzimFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_zzim, container, false);
+        View view = inflater.inflate(
+                R.layout.fragment_zzim, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.zzim_drawer_list);
+
+        adapter = new ZzimListAdapter(getActivity(), getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
     }
 
 
