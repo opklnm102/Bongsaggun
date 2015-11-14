dbee767a22056d3f5ff21ea06f63f4250ac3fc65
andasom <dksektha94@naver.com>
2015-11-14 19:08:22 +0900

    #18 MyPage 구현

--- Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/MyPageFragment.java
+++ Bongsaggun/app/src/main/java/io/j2ffrey_2/bongsaggun/MyPageFragment.java
@@ -9,11 +9,21 @@ import android.view.View;
 
 
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 
 
 public class MyPageFragment extends Fragment {
 
+
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> arrayList;
+    LinearLayout myPageListViewLayout;
 
 
     public static MyPageFragment newInstance() {
@@ -40,8 +50,71 @@ public class MyPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_page, container, false);
+
+
+        myPageListViewLayout = (LinearLayout)view.findViewById(R.id.my_page_listview_container);
+
+
+
+        arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1
+               );
+
+        listView = (ListView) view.findViewById(R.id.my_page_listview);
+
+
+
+
+        listView.setAdapter(arrayAdapter);
+
+
+        arrayAdapter.add("로그인");
+        arrayAdapter.add("로그인");
+        arrayAdapter.add("로그인");
+        arrayAdapter.add("로그인");
+
+
+
+
+        listView.setOnItemClickListener(itemClickListenerOfList);
+
+
+        return view;
     }
+
+
+
+    private AdapterView.OnItemClickListener itemClickListenerOfList = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick(AdapterView<?> adapterView, View clickedView, int pos, long id)
+        {
+
+            if(id==0){
+
+                arrayAdapter.clear();
+
+                arrayAdapter.add("나의 봉사목록");
+                arrayAdapter.add("찜한 봉사활동");
+                arrayAdapter.add("문의");
+                arrayAdapter.add("팀소개");
+                arrayAdapter.add("설정");
+
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+
+
+
+
+
+
+
+        }
+    };
+
+
+
 
 
 
